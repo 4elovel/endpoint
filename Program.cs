1adefd2a9fb722609a86c14ad11d537b8d9ef9ce
4elovel <marcynakvova007@gmail.com>
2023-10-30 16:25:30 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть IP адрес:");
+        string str = Console.ReadLine();
+        try
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(str), 80);
+            Console.WriteLine($"кінцева точка - {endPoint.Address}:{endPoint.Port} створена");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wrong IP format");
+        }
+
+    }
+}
